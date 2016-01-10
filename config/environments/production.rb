e07414a1d7a38af63711a37572a852a4a2bcc6de
Alexander Kehaya <afkehaya@gmail.com>
2016-01-10 10:53:20 -0800

    configed assets compile to true in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Prelaunchr::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'www.viralrails.com' }
 
+  config.assets.compile = true
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5

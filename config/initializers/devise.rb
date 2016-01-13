de87f8a11171ee64e74e52a808bca5fe70406ec6
Alexander Kehaya <afkehaya@gmail.com>
2016-01-13 10:57:54 -0800

    accidental deletion of gemfile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "alex@kehaya.com"
-
+  config.secret_key = ENV["SECRET_KEY_BASE"]
+  
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
